04b1b49614a6894aa6f17485976d5ac6008b918a
joseph-john <joseph@optilogus.com>
2015-03-23 04:11:36 +0530

    Latest Tested Code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-##Put comments here that give an overall description of what your
-##functions do
+## We are try the optmize the processing time it takes for R to compile 
+## inverse of matrix which in general takes lot of resources
+## The idea is not to compile inverse of matrix more than once for a given matrix
 
-##Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This fuction creates a list of functions to set, get, setsolve and getsolve for 
+## a given "SQUARED" Matrix
+makeCacheMatrix <- function(x = matrix()) { 
+  m <- NULL                     
+  set <- function(y){                      
+    x <<- y
+    m <<- NULL              
+  }
+  get <- function() x                           
+  setsolve <- function(solve) m <<- solve   
+  getsolve <- function() m        
+  list(set = set, get = get,                    
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function helps us to cache the inverse for a give SPECIAL matrix
+## Which We created using makeCacheMatrix function
+cacheSolve <- function(x, ...) {                 
+  ma <- x$getsolve()
+  if(!is.null(ma)) {                 
+    message("getting cached inverse matrix data")
+    return(ma)
+  }
+  data <- x$get()                               
+  ma <- solve(data, ...)
+  x$setsolve(ma)
+  ma
 }
